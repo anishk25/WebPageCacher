cc3fbe8e68b950f2f5a8df57c8b8b01fe97a0bd8
Anish Khattar <prgrmer25@gmail.com>
2019-11-30 17:33:31 -0500

    Add indexeddb for storing web pages

--- background.js
+++ background.js
@@ -1,3 +1,16 @@
 chrome.runtime.onInstalled.addListener(function(){
-    
+
+    if (!('indexedDB' in window)) {
+        console.log("This browser doesn't support indexedDB");
+        return;
+    }
+
+    let idb = window.indexedDB;
+    idb.open('webcache-db', 1, function(upgradeDb){
+        // define store for webpages
+        // store will contain webpage url, webpage content and date created
+        if (! upgradeDb.objectStoreNames.contains('webpage-store')) {
+            updgradeDb.createObjectStore('webpage-store', {keypath: 'url'});
+        }
+    });
 });
