961363722b4c2e175562b1809f143f3eeb8967f7
Anish Khattar <prgrmer25@gmail.com>
2019-12-01 12:47:35 -0500

    Get current page url and source

--- popup.js
+++ popup.js
@@ -16,7 +16,39 @@ function documentLoaded() {
     };
 }
 
-function cachePages(depth) {
-    console.log("Cache depth is " + depth);
+async function cachePages(depth) {
+    // let idb = window.indexedDB;
+    // var dbPromise = idb.open('webcache-db', 1);
+
+    var currentUrl = await getUrlOfCurrentPage();
+    var pageSource = await getCurrentPageSource();
 }
 
+
+async function getUrlOfCurrentPage() {
+    var promise = new Promise(function(resolve, reject){
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            resolve(tabs[0].url);
+        });
+    });
+    var url = await promise;
+    return url;
+}
+
+async function getCurrentPageSource() {
+    var promise = new Promise(function(resolve, reject){
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            const scriptToExec = `(${scrapeThePage})()`;
+            chrome.tabs.executeScript(tabs[0].id, {code: scriptToExec}, function(results) {
+                resolve(results[0]);
+            });
+        });
+    })
+    var pageSource = await promise;
+    return pageSource;
+}
+
+function scrapeThePage() {
+    var html_source = document.documentElement.outerHTML;
+    return "<!DOCTYPE html>\n" + html_source;
+}
